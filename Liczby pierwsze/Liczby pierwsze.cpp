ce7fdc31b5acb712cb2856ee1abeb77ba763e76f
Danylo Chekalenko <kotocrydirection@gmail.com>
2022-03-30 11:11:07 +0200

    Algorytm liczb pierwszych

---
+++ Liczby pierwsze/Liczby pierwsze.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int lp = 0;
+	int p = 2;
+	int n;
+	int d;
+	bool a = true;
+
+	cout << "Ile liczb pierwszych chcesz wyświetlić: ";
+	cin >> n;
+
+	while (lp < n)
+	{
+		for (int d = 2 + 1; d <= p - 1; d++)
+		{
+			if (p % d == 0)
+			{
+				p = p + 1;
+				a = false;
+			}
+		}
+		if () {
+			cout << p << endl;
+			lp = lp + 1;
+		}
+		p = p + 1;
+	}
+}
