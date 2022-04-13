a17200faa426d51668046497f87d58b618f8924c
Danylo Chekalenko <kotocrydirection@gmail.com>
2022-04-13 10:54:06 +0200

    [A]

---
+++ Algorytm obliczania NWW oraz NWD/ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int n;
+	int i;
+	int b;
+	cout << "Podaj a: ";
+		cin >> a;
+	cout << "Podaj b: ";
+		cin >> b;
+	i = a * b;
+
+	while(a != b)
+	{
+		if(a < b)
+		{
+			b = b - a;
+		}
+		else
+		{
+			a = a - b;
+		}
+		n = i / a;
+	}
+	cout << "NWD: " << a;
+	cout << "NWW: " << n;
+}
